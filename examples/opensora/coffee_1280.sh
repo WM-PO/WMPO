d1ce279a066322a0823f15387b819206ffd50f83
zhufangqi <zhufangqi@bytedance.com>
2025-11-09 09:34:27 +0000

    update WM training scripts

---
+++ examples/opensora/coffee_1280.sh
@@ -0,0 +1,15 @@
+GPUS_PER_NODE=8
+MASTER_ADDR=xx.xx.xx.xx:xxxx
+NNODES=4
+RANK=0
+
+CONFIG=${1:-"checkpoint_files/world_models/coffee/P_1280/train_mimicgen_cfg.py"}
+
+torchrun \
+    --nproc_per_node $GPUS_PER_NODE \
+    --nnodes $NNODES \
+    --node_rank $RANK \
+    --rdzv_endpoint $MASTER_ADDR \
+    --rdzv_backend c10d \
+    dependencies/opensora/scripts/train_web.py \
+    $CONFIG \
